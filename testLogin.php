2b63edcfb6c80c1c1ba5b4d2de2a8db08bd8507c
GabrielBento299 <gabrieljesus100-2010@hotmail.com>
2022-03-12 15:17:11 -0300

    login screen done.

---
+++ testLogin.php
@@ -0,0 +1,24 @@
+<?php 
+
+    // print_r($_REQUEST);
+    
+    if(isset($_POST['submit']) && !empty($_POST['email']) 
+                               && !empty($_POST['senha']) ){
+        // ACESSA
+
+        include_once('config.php');
+
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        
+        print_r('Email: ' . $email);
+        print_r('<br>');
+        print_r('senha: ' . $senha);
+
+        
+    }else {
+        // NAO ACESSA
+        header('Location: login.html');
+    }
+
+?>
